2a67b044dbdc36346567fbf8c33a82b8b471dc4f
Trever O <toveson9522@gmail.com>
2023-05-13 23:23:21 -0600

    file structure clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-import "./App.css"
-import logo from "./logo.svg"
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+export default function App() {
+  return <h1>Hello from app.tsx</h1>
 }
-
-export default App
