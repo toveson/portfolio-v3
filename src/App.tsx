4a86800ff148be1ffac12ec243111e11bd1d37a3
Trever O <toveson9522@gmail.com>
2023-05-15 19:42:15 -0600

    added css baseline

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ export default function App() {
   const [currentPage, setCurrentPage] = useState<string>("Home")
 
   return (
-    // TODO: this will have view called and routing
     // ? Set up useContext here?
     <>
       <NavBar setCurrentPage={setCurrentPage} currentPage={currentPage} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import CssBaseline from "@mui/material/CssBaseline"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
@@ -5,6 +6,7 @@ import App from "./App"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
