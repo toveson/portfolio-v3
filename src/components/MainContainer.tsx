21ee8690ec63fc41300fe6477676f8998becafca
Trever O <toveson9522@gmail.com>
2023-10-06 02:45:17 -0600

    added margin to the top and bottom of MainContainer only when there is overflow

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,4 +1,5 @@
 import {Card, CardContent} from "@mui/material"
+import {useEffect, useRef, useState} from "react"
 
 export interface MainContainerProps {
   content: any
@@ -6,18 +7,36 @@ export interface MainContainerProps {
 
 export const MainContainer: React.FC<MainContainerProps> = (props) => {
   const {content} = props
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [hasOverflow, setHasOverflow] = useState(false)
+
+  useEffect(() => {
+    const containerElement = containerRef.current
+    if (containerElement) {
+      setHasOverflow(
+        containerElement.scrollHeight > containerElement.clientHeight
+      )
+    }
+  }, [])
 
   return (
     <div
+      ref={containerRef}
       style={{
         display: "flex",
         justifyContent: "center",
         height: "100%",
         alignItems: "center",
-        backgroundColor: "gray"
+        backgroundColor: "gray",
+        overflow: "auto",
+        padding: "0"
       }}
     >
-      <Card style={{maxWidth: "75vw"}}>
+      <Card
+        style={{maxWidth: "75vw"}}
+        // adds margin to the top and bottom when there is overflow
+        sx={hasOverflow ? {mt: 18, mb: 4} : {}}
+      >
         <CardContent>{content}</CardContent>
       </Card>
     </div>
