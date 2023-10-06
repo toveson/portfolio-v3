c849e8381c7f263d80c8651dfd69e360562c155b
Trever O <toveson9522@gmail.com>
2023-10-05 22:02:37 -0600

    added home page content

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,19 +1,69 @@
-import ConstructionIcon from "@mui/icons-material/Construction"
+// import ConstructionIcon from "@mui/icons-material/Construction"
+import {Avatar, Stack, Typography} from "@mui/material"
+import {useState} from "react"
+import Typed from "react-typed"
+import avatar from "../assets/images/trever-avatar.png"
 import {MainContainer} from "../components/MainContainer"
 
 export const Home: React.FC = (props) => {
+  const [nameTyped, setNameTyped] = useState(true)
+  const [skillTyped, setSkillTyped] = useState(true)
+
   return (
     <MainContainer
       content={
         <>
-          <div style={{display: "flex", justifyContent: "center"}}>
-            <ConstructionIcon
+          <Stack style={{justifyContent: "center", alignItems: "center"}}>
+            <Avatar
               style={{
-                fontSize: 400
+                width: "75%",
+                height: "75%",
+                margin: "1rem"
               }}
+              src={avatar}
+              alt="Trever's avatar"
             />
-          </div>
-          <h1> Home is currently under construction</h1>
+          </Stack>
+          <Stack style={{justifyContent: "center", alignItems: "center"}}>
+            <Typography variant="h4">
+              <Typed
+                strings={["Hi, I'm Trever Oveson"]}
+                typeSpeed={70}
+                onComplete={() => setNameTyped(false)}
+                showCursor={nameTyped}
+              />
+            </Typography>
+            <Typography variant="h5">
+              <Typed
+                strings={["Web Development Skills:"]}
+                typeSpeed={100}
+                startDelay={2000}
+                onComplete={() => setSkillTyped(false)}
+                showCursor={skillTyped}
+              />
+            </Typography>
+            <Typography variant="h5">
+              <Typed
+                // showCursor={false}
+                strings={[
+                  "CSS",
+                  "Git",
+                  "HTML",
+                  "JavaScript",
+                  "MUI",
+                  "Node",
+                  "React",
+                  "TypeScript"
+                ]}
+                startDelay={5000}
+                typeSpeed={70}
+                backSpeed={40}
+                backDelay={1000}
+                shuffle={true}
+                loop
+              />
+            </Typography>
+          </Stack>
         </>
       }
     />
