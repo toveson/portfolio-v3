0e91b38294a88c0ca7158e598a522ce4b6e0f957
Trever O <toveson9522@gmail.com>
2023-10-05 22:36:23 -0600

    changed package for typing animation

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,8 @@
 // import ConstructionIcon from "@mui/icons-material/Construction"
 import {Avatar, Stack, Typography} from "@mui/material"
 import {useState} from "react"
-import Typed from "react-typed"
+// import Typed from "react-typed"
+import {TypeAnimation} from "react-type-animation"
 import avatar from "../assets/images/trever-avatar.png"
 import {MainContainer} from "../components/MainContainer"
 
@@ -26,24 +27,34 @@ export const Home: React.FC = (props) => {
           </Stack>
           <Stack style={{justifyContent: "center", alignItems: "center"}}>
             <Typography variant="h4">
-              <Typed
+              {/* <Typed
                 strings={["Hi, I'm Trever Oveson"]}
                 typeSpeed={70}
                 onComplete={() => setNameTyped(false)}
                 showCursor={nameTyped}
+              /> */}
+              <TypeAnimation
+                sequence={["Hi, I'm Trever Oveson"]}
+                wrapper="span"
+                speed={10}
               />
             </Typography>
             <Typography variant="h5">
-              <Typed
+              {/* <Typed
                 strings={["Web Development Skills:"]}
                 typeSpeed={100}
                 startDelay={2000}
                 onComplete={() => setSkillTyped(false)}
                 showCursor={skillTyped}
+              /> */}
+              <TypeAnimation
+                sequence={[2000, "Web Development Skills:"]}
+                wrapper="span"
+                speed={4}
               />
             </Typography>
             <Typography variant="h5">
-              <Typed
+              {/* <Typed
                 // showCursor={false}
                 strings={[
                   "CSS",
@@ -61,6 +72,29 @@ export const Home: React.FC = (props) => {
                 backDelay={1000}
                 shuffle={true}
                 loop
+              /> */}
+              <TypeAnimation
+                sequence={[
+                  5000,
+                  "CSS",
+                  1000,
+                  "Git",
+                  1000,
+                  "HTML",
+                  1000,
+                  "JavaScript",
+                  1000,
+                  "MUI",
+                  1000,
+                  "Node",
+                  1000,
+                  "React",
+                  1000,
+                  "TypeScript"
+                ]}
+                wrapper="span"
+                speed={2}
+                repeat={Infinity}
               />
             </Typography>
           </Stack>
